a567f8ff9f952bb04696ce63f03b9c633749e44e
Ikmal <>
2024-10-28 00:35:48 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # android-compose-modularization
+
+https://www.youtube.com/watch?v=4yz8pTCqkPo&ab_channel=Landofcoding
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "android-compose-modularization"
 include(":app")
- 
