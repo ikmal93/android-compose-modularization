fe016576d402fa6a22ab42f3cc1098e58aeb4dda
Ikmal <>
2024-10-28 01:11:32 +0700

    Add module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,4 +20,17 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "android-compose-modularization"
+
 include(":app")
+
+include(":core:analytics")
+include(":core:data")
+include(":core:database")
+include(":core:domain")
+include(":core:network")
+include(":core:notifications")
+
+include(":feature:home")
+include(":feature:login")
+include(":feature:onboarding")
+include(":feature:register")
